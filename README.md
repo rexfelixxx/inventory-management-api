ccd8e4587f1d7766f934cd19346af6d0d9de2e05
sadonat <setialana49@gmail.com>
2026-08-23 12:29:23 +0700

    Redokumentasi:README.md

--- README.md
+++ README.md
@@ -1,39 +1,65 @@
 # INVENTORY MANAGEMENT API
 
-## Overview
+ API simpel yang dibuat dengan penuh cinta oleh [sadonat🍩](https://github.com/sadonat).
 
-API simpel yang dibuat dengan penuh cinta oleh [sadonat](https://github.com/sadonat)
 ini adalah dokumentasi umum, untuk dokumentasi teknis silahkan lihat [docs](docs/table_of_contents.md).
-
-## Features
-
-- Login
-- Token Authorization (Database Based)
-- Users CRUD
-- Tokens CRUD
-- Category CRUD
 
 ## Techstack
 
 - **Backend**: PHP
 - **Database**: MariaDB (see [docs/database](docs/database.md))
 
-## Installation
+## How to Use
+### LOKAL
+    Menjalankan semuanya di mesinmu sendiri.
+**Persyaratan**:
+- PHP 8.2+
+- PHP PDO Extension
+- PHP `pdo_mysql` Driver
+- MariaDB 10.6+
 
-- Clone repository ini ke folder server PHP milikmu.
-- Buat database baru
-- Import `database.sql` ke database yang baru kamu buat
-- Jalankan server PHP milikmu
+**Cara Instalasi**:
+- Clone repository ini dengan menjalankan perintah ini
+```sh
+git clone https://github.com/sadonat/inventory-management-api.git
+```
+- Masuk ke dalam folder `inventory-management-api` tersebut.
+- Pastikan mariadb sudah berjalan, kalau belum,  jalankan perintah ini
+```sh
+maridbd-safe &
+```
+- Import skema database dengan command ini. Kamu boleh menggunakan user manapun yang kamu mau.
+```sh
+mariadb -u root -p < database.sql
+```
+- Masuk ke mariadb
+```sh
+mariadb -u root -p
+```
+- Jalankan ini jika belum membuat user
+```sql
+CREATE USER 'nama_user'@'localhost' IDENTIFIED BY 'password';
+```
+- Jalankan ini jika belum memberi user yang kamu tentukan privileges untuk mengakses database `stockdb`
+```sql
+GRANT ALL PRIVILEGES ON stockdb.* TO 'nama_user'@'localhost';
+```
+- Sesuaikan file konfigurasi database sesuai milikmu di [Database.php](configs/Database.php). Dalam kasus umum kamu cukup mengganti `$user` dan `$password` sesuai user yang kamu beri privilege untuk stockdb
+```php
+<?php
+$host = '127.0.0.1';
+$db = 'stockdb';
+$user = 'stock_admin';
+$pass = 'admin1234';
+$charset = 'utf8mb4';
+```
+- Jalankan server PHP milikmu dengan
+```sh
+php -S localhost:8000
+```
+- Selamat sekarang API ini berjalan di mesinmu, untuk login awal ke API nya. gunakan username `admin` dan password `1234`
 
 ## Usage
 
 Kamu bisa menggunakan program ini setelah menjalankannya.Karena program ini hanyalah API, kamu perlu membuat interface sendiri / menggunakan API tester seperti Postman dan Hopscotch.
 see [docs/api-usage](docs/api-usage/table_of_contents.md)
-
-## Folder Structure
-
-- **routes/** -> routing
-- **controllers/** -> implementasi logika
-- **models/** -> implementasi pengolahan database
-- **configs/** -> file-file konfigurasi
-- **helpers/** -> alat-alat
---
+++ database.sql
@@ -0,0 +1,131 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-12.3.2-MariaDB, for Android (aarch64)
+--
+-- Host: localhost    Database: stockdb
+-- ------------------------------------------------------
+-- Server version	12.3.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Current Database: `stockdb`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `stockdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+
+USE `stockdb`;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sku` varchar(30) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `description` tinytext DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sku` (`sku`),
+  KEY `fk_category` (`category_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `stock_movement`
+--
+
+DROP TABLE IF EXISTS `stock_movement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock_movement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) DEFAULT NULL,
+  `action` enum('in','out','adjustment') DEFAULT 'adjustment',
+  `quantity` int(11) NOT NULL,
+  `description` tinytext NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `move_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_item` (`item_id`),
+  KEY `fk_user` (`user_id`),
+  CONSTRAINT `fk_item` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `token` (
+  `user_id` int(11) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `expired_at` timestamp NOT NULL,
+  KEY `fk_user` (`user_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('staff','admin') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+INSERT INTO user (id, name, password, role) VALUES (0, 'admin', '1234', 'admin');
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2026-08-22 15:23:22
